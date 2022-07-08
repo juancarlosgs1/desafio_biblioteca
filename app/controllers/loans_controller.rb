f83e3233e49ec55e3e4f29a724bc449c49383c8f
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-07-07 21:18:20 -0400

    actualizar navbar

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -27,10 +27,27 @@ class LoansController < ApplicationController
 
     @loan.user_name = params[:user_name]
 
-    if @loan.save
-      redirect_to new_loan_path , notice: 'Loan was successfully created'
+    if (@loan.book.state_book == "en_estante" && @loan.state_book == "prestamo") || (@loan.book.state_book == "prestado" && @loan.state_book == "devolucion")
+      if @loan.save
+
+        @book = @loan.book
+
+        if @loan.state_book == "prestamo"
+          @book.state_book = "prestado"
+        else
+          @book.state_book = "en_estante"
+        end 
+
+        @book.save
+
+        redirect_to new_loan_path , notice: 'Loan was successfully created'
+      else
+        redirect_to new_loan_path , notice: 'ERROR !!!!!!!'
+      end
     else
-      redirect_to new_loan_path , notice: 'ERROR !!!!!!!'
+      # if @loan.state_book == "prestamo"
+        redirect_to new_loan_path , notice: 'No es posible realizar este registro'
+      # end
     end
 
       # @loan = Loan.create(book_id: params[:books].to_i , state_book: params[:state_book],  )
